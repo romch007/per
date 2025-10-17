aaef3e8abe4be9571e1c52b42b05cd547eacff41
Romain Chardiny <romain.chardiny@gmail.com>
2025-10-17 15:31:46 +0200

    use debug printing

--- PERDriver/Driver.c
+++ PERDriver/Driver.c
@@ -3,14 +3,15 @@
 NTSTATUS DriverUnload(_In_ PDRIVER_OBJECT driverObject) {
     UNREFERENCED_PARAMETER(driverObject);
 
-    KdPrint(("Goodbye World!\n"));
+    DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "PER driver unloaded");
+
     return STATUS_SUCCESS;
 }
 
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT driverObject, _In_ PUNICODE_STRING registryPath) {
     UNREFERENCED_PARAMETER(registryPath);
     
-    KdPrint(("Hello World!\n"));
+    DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "PER driver loaded");
     driverObject->DriverUnload = DriverUnload;
 
     return STATUS_SUCCESS;
