c8a7c7264483ea5e1552cc05809e919adad841ba
Romain Chardiny <romain.chardiny@gmail.com>
2026-01-16 12:50:32 +0100

    copy driver from shared folder in install script

--- scripts/installdriver.ps1
+++ scripts/installdriver.ps1
@@ -1,8 +1,12 @@
 $name = "VulnerableDriver"
+$sharedPath = "Z:\VulnerableDriver.sys"
 $path = "C:\Users\WDKRemoteUser\Downloads\VulnerableDriver.sys"
 
 sc.exe stop $name
 sc.exe delete $name
+
+Write-Host "Copying driver to $path"
+copy $sharedPath $path
 
 sc.exe create $name type= kernel start= demand binPath= $path
 sc.exe start $name
